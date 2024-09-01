fc392c7c1aae3fc7888d2d3719b00f0a19afa115
VivekJaiswal18 <96608169+VivekJaiswal18@users.noreply.github.com>
2024-09-02 03:51:47 +0530

    Readme Update

--- src/app/gated-content/page.tsx
+++ src/app/gated-content/page.tsx
@@ -42,11 +42,22 @@ const MustLogin = () => (
 const reason = "you do not own any NFT"; 
 
 const NotAllowed = () => (
-  <div className="text-center justify-center font-montserrat">
+  <div className="text-center mt-16 justify-center items-center flex-col min-h-screen font-bold font-montserrat mb-6">
+
+    <div className="text-2xl ">
     You are logged in but you do not have access to this page because {reason}
-    <Link href="/">
-    <button>Go To Login Page</button></Link>
-    <Link href="/claim-nft">
-    <button>Go To Claim NFT</button></Link>
+    </div>
+    <div className="p-4 flex items-center justify-center mb-3 flex-col">
+    <Link  href={"/"}>
+            <button className="mt-4 bg-zinc-100 text-black px-4 py-2 rounded-md">
+            Go To Login Page
+            </button>
+        </Link>
+        <Link  href={"/claim-nft"}>
+            <button className="mt-4 bg-zinc-100 text-black px-4 py-2 rounded-md">
+            Go To Claim NFT
+            </button>
+        </Link>
+  </div>
   </div>
 );
